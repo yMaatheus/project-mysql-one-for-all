3860ffc5eb720268f944759444fa47c065e275ce
yMaatheus <stvgamer74@gmail.com>
2022-06-07 00:53:24 -0300

    feat(R02): contando a quantidade total de artistas, albuns e canções

---
+++ desafio2.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(DISTINCT can.cancao_id) AS cancoes, COUNT(DISTINCT art.artista_id) AS artistas, COUNT(DISTINCT alb.album_id) AS albuns FROM SpotifyClone.cancoes AS can JOIN SpotifyClone.artists AS art JOIN SpotifyClone.albuns AS alb;
